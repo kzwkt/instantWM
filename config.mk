2a0d6547e949986537994690eb672637622856d0
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-23 10:24:18 +0000

    Update config.mk

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 # instantWM version
-CMS_VERSION := $(shell git describe --abbrev=5 --dirty=-dev --always --tags 2>/dev/null || echo "unknown")
-VERSION := "instantOS $(CMS_VERSION) - Build $(shell LANG=en_us_8859_1 date '+%a, %b %e %Y, %R:%S %z') on $(shell hostname 2>/dev/null || echo "unknown host")"
+CMS_VERSION := "1"
+VERSION := "1"
 
 # Customize below to fit your system
 
